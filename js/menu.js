317c845532b75c4250bba7cd04c1f37ffe0fc6eb
Paiaro <gabrielpaiarop@gmail.com>
2024-05-08 14:04:43 -0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,8 @@
 'use strict';
 
-const botaoMenu = document.querySelector("nav h2 a");
-const listaDeLinks = document.querySelector(".links-menu");
-botaoMenu.addEventListener("click", function (event) {
-    event.preventDefault();
-    listaDeLinks.classList.toggle("aberto");
-    if (listaDeLinks.classList.contains("aberto")) {
-        botaoMenu.innerHTML = "Fechar &times;";
-    } else {
-        botaoMenu.innerHTML = "Menu &equiv;";
-    }
+const menuBtn = document.querySelector('#menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('aberto');
 });
